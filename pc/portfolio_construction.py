6077d54471ba93fa3d506728bac5b92ca3b8c095
tlorans <thomaslorans@gmail.com>
2024-07-22 14:42:44 +0200

    test

--- pc/portfolio_construction.py
+++ pc/portfolio_construction.py
@@ -3,71 +3,73 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# FastAPI endpoint URL
-base_url = "http://127.0.0.1:8000/api/v1"
 
-# Function to get all fund names
-def get_fund_names():
-    try:
-        response = requests.get(f"{base_url}/funds/")
-        response.raise_for_status()
-        fund_names = response.json()
-        return fund_names
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error fetching fund names: {e}")
-        return []
+st.write("Page under construction")
+# # FastAPI endpoint URL
+# base_url = "http://127.0.0.1:8000/api/v1"
 
-# Function to get fund holdings
-def get_fund_holdings(fund_id):
-    try:
-        response = requests.get(f"{base_url}/funds/{fund_id}/holdings")
-        response.raise_for_status()
-        holdings = response.json()
-        return holdings
-    except requests.exceptions.RequestException as e:
-        # st.error(f"Error fetching fund holdings: {e}")
-        return []
+# # Function to get all fund names
+# def get_fund_names():
+#     try:
+#         response = requests.get(f"{base_url}/funds/")
+#         response.raise_for_status()
+#         fund_names = response.json()
+#         return fund_names
+#     except requests.exceptions.RequestException as e:
+#         st.error(f"Error fetching fund names: {e}")
+#         return []
 
-# Function to get fund true returns
-def get_fund_true_returns(fund_id):
-    try:
-        response = requests.get(f"{base_url}/funds/{fund_id}/total_returns")
-        response.raise_for_status()
-        true_returns = response.json()
-        return true_returns
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error fetching fund true returns: {e}")
-        return []
+# # Function to get fund holdings
+# def get_fund_holdings(fund_id):
+#     try:
+#         response = requests.get(f"{base_url}/funds/{fund_id}/holdings")
+#         response.raise_for_status()
+#         holdings = response.json()
+#         return holdings
+#     except requests.exceptions.RequestException as e:
+#         # st.error(f"Error fetching fund holdings: {e}")
+#         return []
 
-st.title("Portfolio Construction")
+# # Function to get fund true returns
+# def get_fund_true_returns(fund_id):
+#     try:
+#         response = requests.get(f"{base_url}/funds/{fund_id}/total_returns")
+#         response.raise_for_status()
+#         true_returns = response.json()
+#         return true_returns
+#     except requests.exceptions.RequestException as e:
+#         st.error(f"Error fetching fund true returns: {e}")
+#         return []
 
-# Get all fund names
-fund_names = get_fund_names()
-selected_fund_name = st.selectbox("Select a fund", fund_names)
+# st.title("Portfolio Construction")
 
-if selected_fund_name:
-    # Fetch fund details
-    response = requests.get(f"{base_url}/funds/{selected_fund_name}")
-    fund = response.json()
-    fund_id = fund['id']
+# # Get all fund names
+# fund_names = get_fund_names()
+# selected_fund_name = st.selectbox("Select a fund", fund_names)
 
-    # st.subheader("Fund Details")
-    # st.write(f"Name: {fund['name']}")
-    # st.write(f"Fund Share Class ID: {fund['fund_share_class_id']}")
+# if selected_fund_name:
+#     # Fetch fund details
+#     response = requests.get(f"{base_url}/funds/{selected_fund_name}")
+#     fund = response.json()
+#     fund_id = fund['id']
 
-    # Fetch and display fund holdings
-    holdings = get_fund_holdings(fund_id)
-    st.subheader("Fund Holdings")
-    if holdings:
-        holdings_df = pd.DataFrame(holdings)
-        st.dataframe(holdings_df)
+#     # st.subheader("Fund Details")
+#     # st.write(f"Name: {fund['name']}")
+#     # st.write(f"Fund Share Class ID: {fund['fund_share_class_id']}")
 
-    # Fetch and plot fund true returns
-    true_returns = get_fund_true_returns(fund_id)
-    st.subheader("Fund Total Returns")
-    if true_returns:
-        returns_df = pd.DataFrame(true_returns)
-        returns_df['date'] = pd.to_datetime(returns_df['date'])
-        returns_df = returns_df.set_index('date')
+#     # Fetch and display fund holdings
+#     holdings = get_fund_holdings(fund_id)
+#     st.subheader("Fund Holdings")
+#     if holdings:
+#         holdings_df = pd.DataFrame(holdings)
+#         st.dataframe(holdings_df)
+
+#     # Fetch and plot fund true returns
+#     true_returns = get_fund_true_returns(fund_id)
+#     st.subheader("Fund Total Returns")
+#     if true_returns:
+#         returns_df = pd.DataFrame(true_returns)
+#         returns_df['date'] = pd.to_datetime(returns_df['date'])
+#         returns_df = returns_df.set_index('date')
         
-        st.line_chart(returns_df['total_return'])
+#         st.line_chart(returns_df['total_return'])
