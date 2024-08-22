971d46e54f251852c0dbd099ca1f4092e2816c42
Thomas Lorans <thomaslorans@gmail.com>
2024-08-22 09:28:10 +0200

    starting

--- emp/capm.py
+++ emp/capm.py
@@ -4,9 +4,29 @@ from typing import List
 import io
 import contextlib
 
-# Title of the web app
-st.title("Testing the CAPM Beta")
+def run_code_and_capture_output(code: str) -> str:
+    buffer = io.StringIO()
+    with contextlib.redirect_stdout(buffer):
+        try:
+            exec(code, globals(), locals())
+        except Exception as e:
+            print(e)
+    return buffer.getvalue()
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+# Title of the web app
+st.markdown("""
+            # Testing the CAPM
+
+            To check empirically the validity of the theory we have discussed, the first thing to test is the validity of the CAPM, which forms the basis of the theory.            
+            As we have seen, cross-sectional variation in expected returns should be explained by the covariance between the excess return 
+            of the asset and the excess return of the market portfolio. 
+            The regression coefficient of this relationship is the market beta of the asset.
+            In this part, we will first start by estimating the beta of the assets, and then apply portfolio univariate sort to test the CAPM.
+            
+            """)
 
 st.markdown("""
 ## Estimating the CAPM Beta
