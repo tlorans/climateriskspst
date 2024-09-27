ff686726f7dfa0ebb2c43a0bfbe206e0372011f8
tlorans <thomaslorans@gmail.com>
2024-09-27 14:43:03 +0200

    beginning with rewarded vs unrewarded

--- rewarded_unrewarded/rewarded_unrewarded.py
+++ rewarded_unrewarded/rewarded_unrewarded.py
@@ -10,8 +10,26 @@ st.title('Rewarded and Unrewarded Risk in Finance')
 st.subheader('Simple One-Period Two-Factor Model')
 
 st.write(r"""
-Explain simple two-factor structure from Daniel (2020)
+Following Daniel $\textit {et al.}$ (2020), 
+we consider a one-period economy with $N$ assets. 
+Realized excess returns are given by:
 """)
+
+st.latex(r"""
+        \begin{equation}
+r_i = \beta_i (f + \lambda) + \gamma_i g + \epsilon_i
+\end{equation}
+         """)
+
+st.write(r"""
+with $r_i$ the excess return of asset $i$, 
+$\beta_i$ the factor loading on the rewarded factor $f$,
+$\lambda$ the risk premium on $f$,
+$\gamma_i$ the factor loading on the unrewarded factor $g$,
+$\epsilon_i$ the idiosyncratic return. 
+We have 
+$\mathbb{E}[\epsilon_i] = \mathbb{E}[f] = \mathbb{E}[g] = 0$.
+         """)
 
 st.subheader('What About Climate Risks?')
 
