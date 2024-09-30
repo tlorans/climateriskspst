1d5b3a4e56b48f1729c2f2130e301a3c5e774bef
tlorans <thomaslorans@gmail.com>
2024-09-30 14:41:03 +0200

    ok

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 if "page" not in st.session_state:
     st.session_state.page = None
 
-PAGES = ["Rewarded and Unreward Risks","Unexpected Returns","Hedging Unrewarded Risks"]
+PAGES = ["Rewarded and Unreward Risks","Hedging Unrewarded Risks"]
 
 def set_page(page):
     st.session_state.page = page
@@ -14,8 +14,6 @@ def home_page():
     st.write("Please choose a section to navigate:")
     if st.button("Rewarded and Unrewarded Risks"):
         set_page("Rewarded and Unrewarded Risks")
-    if st.button("Unexpected Returns"):
-        set_page("Unexpected Returns")
     if st.button("Hedging Unrewarded Risks"):
         set_page("Hedging Unrewarded Risks")
 
@@ -33,12 +31,7 @@ first_part = [st.Page("rewarded_unrewarded/what_are_climate_risks.py", title = "
               st.Page("rewarded_unrewarded/project.py", title = "Project 1"),
 ]
 
-second_part = [st.Page("climate_risks_unexpected_returns/theory.py", title = "Theory", default=(page=="Unexpected Returns")),
-                st.Page("climate_risks_unexpected_returns/empirics.py", title = "Empirics"),
-                st.Page("climate_risks_unexpected_returns/project.py", title = "Project 3")
-    ]
-
-third_part = [st.Page("hedging_unrewarded_risks/targeted.py", title = "Hedging Portfolio", default=(page=="Hedging Unrewarded Risks")),
+second_part = [st.Page("hedging_unrewarded_risks/targeted.py", title = "Hedging Portfolio", default=(page=="Hedging Unrewarded Risks")),
                st.Page("hedging_unrewarded_risks/project.py", title = "Project 4"),
 ]
 
@@ -50,10 +43,8 @@ page_dict = {}
 
 if st.session_state.page == "Rewarded and Unrewarded Risks":
     page_dict["Rewarded and Unrewarded Risks"] = first_part
-elif st.session_state.page == "Unexpected Returns":
-    page_dict["Unexpected Returns"] = second_part
 elif st.session_state.page == "Hedging Unrewarded Risks":
-    page_dict["Hedging Unrewarded Risks"] = third_part
+    page_dict["Hedging Unrewarded Risks"] = second_part
 
 
 if len(page_dict) > 0:
