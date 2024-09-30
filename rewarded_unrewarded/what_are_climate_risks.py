5fc015997bede327f408625f28cdd01b6c82cc93
tlorans <thomaslorans@gmail.com>
2024-09-30 10:07:55 +0200

    we will have first page of intro

--- rewarded_unrewarded/what_are_climate_risks.py
+++ rewarded_unrewarded/what_are_climate_risks.py
@@ -7,11 +7,44 @@ import sympy.stats as stats
 import plotly.graph_objs as go
 
 
-st.title('Climate Risks: What Are We Talking About?')
+st.title('Climate Risks and the Cross Section of Stock Returns')
 
 
 st.write(r"""
-Use Venturini review
+Two main currents characterise the literatuer 
+on climate finance. The first relates 
+to climate change science and the related risks. 
+The second describes the financial implications
+         for asset pricing.
+
+Climate change risks can be divided into physical and transition risks 
+(Carney, 2015). Physical risks refer to the negative impact 
+of climate and weather-related events on company operations or society 
+         (Tankov and Tantet, 2019). 
+We traditionaly think of these risks as two types:
+         (i) acute physical risks, that are related 
+         to extreme weather events, and (ii) chronic physical risks,
+         that are related to long-term shifts in climate patterns or sea-level rise.
+
+On the other hand, transition risks refer to all 
+         possible scenarios coherent with a path to a low-carbon 
+         economy and all related implications for fossil fuels 
+         and dependent sectors (Curtin $\textit{et al.}$, 2019).
+
+In this course, we will focus on transition risks,
+and discuss how their dynamics matter for asset pricing.
          """)
+
+st.write(r"""
+With respect to the financial side, climate finance 
+         literature can be divided according to the type 
+         of financial risks and asset considered.
+In this course, we will focus on the impact on equity.
+         """)
+
+st.subheader('Physical Risks Channel')
+
+
+st.subheader('Transition Risks Channel')
 
 
