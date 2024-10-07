ecf3c164ed0ef5efb4828dc56502544766c72872
tlorans <thomaslorans@gmail.com>
2024-10-07 16:50:16 +0200

    advances on the hedging portfolio stuff

--- hedging_unrewarded_risks/combination.py
+++ hedging_unrewarded_risks/combination.py
@@ -221,146 +221,33 @@ st.write(r"""
 Play around with the slider to find a combination of the initial portfolio $c$ and the hedging portfolio $h$ that minimizes the variance of the portfolio $p$.
          """)
 
-# # Convert beta and gamma inputs to Sympy matrices
-# gamma = sp.Matrix(selected_gamma)
-# # Convert beta inputs to Sympy matrices
-# beta = sp.Matrix(fixed_beta)
 
-# # Convert beta and gamma inputs to NumPy arrays
-# beta_np = np.array(fixed_beta)
-# gamma_np = np.array(selected_gamma)
+st.subheader('Optimal Hedge Ratio')
 
-
-# # Get the indices of the sorted beta values
-# sorted_indices = np.argsort(beta_np)
-
-# # Use SymPy's Rational to keep weights as fractions
-# w_long = sp.Matrix([0]*N)
-# w_short = sp.Matrix([0]*N)
-
-# # Assign long positions (1/3) to the top 3 assets
-# for idx in sorted_indices[-6:]:
-#     w_long[idx] =sp.Rational(1, 6)
-
-# # Assign short positions (-1/3) to the bottom 3 assets
-# for idx in sorted_indices[:6]:
-#     w_short[idx] = sp.Rational(-1, 6)
-
-# # Combine long and short positions to form the final weight vector
-# w_c = w_long + w_short
+st.write(r"""
+Daniel $\textit{et al.}$ (2020) showed that we can do better by combining the hedge portfolio $h$ with the characteristic portfolio $c$ in 
+order to maximize the Sharpe ratio of the portfolio $p$. Given that the hedge portfolio has zero expected excess return, this is equivalent to finding the 
+combination of the characteristic portfolio and the hedge portfolio that minimizes the variance of the resultant portfolio, that is:
+         """)
 
 
-# # Get the top 3 (high beta) and bottom 3 (low beta) indices
-# high_beta_indices = sorted_indices[-6:]  # Indices for high beta
-# low_beta_indices = sorted_indices[:6]    # Indices for low beta
 
-# low_beta_high_gamma_sorted = low_beta_indices[np.argsort(gamma_np[low_beta_indices])][-3:]
+#use correlation coefficient as the optimal hedge ratio
+optimal_hedge_ratio = variance_g_c / variance_g_h
 
-# low_beta_low_gamma_sorted = low_beta_indices[np.argsort(gamma_np[low_beta_indices])][:3]
+st.latex(r"""
+\begin{equation}
+\min_{\delta} \text{var}(r_c - \delta r_h) \implies \delta^* = \rho_{c,h}\frac{\sigma_c}{\sigma_h}
+\end{equation}
+""")
+st.write(fr"""The calculated value of the optimal hedge ratio is: $\delta^* = {sp.latex(optimal_hedge_ratio.simplify())}$.""")
 
-# high_beta_high_gamma_sorted = high_beta_indices[np.argsort(gamma_np[high_beta_indices])][-3:]
+# Variance of the combined portfolio
+variance_p_optim = variance_c - optimal_hedge_ratio * variance_h
 
-# high_beta_low_gamma_sorted = high_beta_indices[np.argsort(gamma_np[high_beta_indices])][:3]
+st.write(r"""
+         The variance of the combined portfolio $p$ is:
+         """)
+st.latex(f"\\sigma^2_p = {sp.latex(variance_p_optim)}")
 
-# # Combine the long and short positions
-# long = np.concatenate([low_beta_high_gamma_sorted, high_beta_high_gamma_sorted])
-# short = np.concatenate([low_beta_low_gamma_sorted, high_beta_low_gamma_sorted])
-
-# # Use SymPy's Rational to keep weights as fractions
-# w_long = sp.Matrix([0] * N)
-# w_short = sp.Matrix([0] * N)
-
-# # Assign long positions (1/3) to the selected assets
-# for idx in long:
-#     w_long[idx] = sp.Rational(1, 6)
-
-# # Assign short positions (-1/3) to the selected assets
-# for idx in short:
-#     w_short[idx] = sp.Rational(-1, 6)
-
-# # Combine long and short positions to form the final weight vector
-# w_h = w_long + w_short
-
-# w = w_c - sp.Rational(1, 3) * w_h
-
-# st.write(r"""
-#          The weights of the portfolio are:
-#          """)
-
-# # Prepare weights in LaTeX format as a row vector
-# weights_latex = r"\begin{bmatrix} "
-# for i in range(N):
-#     weights_latex += f"{w[i]} & "
-# weights_latex = weights_latex[:-2] + r" \end{bmatrix}"  # Remove the last "&" and close the matrix
-
-# st.latex(r"""
-# w_p^T = """ + weights_latex)
-
-
-# # Define priced factor as normal random variable with variance properties
-# f = stats.Normal('f', 0, sp.symbols('sigma_f'))  # Priced factor f with E[f] = 0 and var(f) = sigma_f^2
-# # Characteristic premium
-# lambda_ = sp.symbols('lambda')
-# # Define idiosyncratic errors epsilon_i as random variables with zero mean and variance sigma_epsilon^2
-# epsilon = sp.Matrix([stats.Normal(f'epsilon', 0, sp.symbols('sigma_epsilon')) for i in range(N)])
-# # Define priced and unpriced factors as normal random variables with variance properties
-# g = stats.Normal('g', 0, sp.symbols('sigma_g'))  # Unpriced factor g with E[g] = 0 and var(g) = sigma_g^2
-
-# # Define symbols for variances of the factors and idiosyncratic error
-# sigma_g = sp.symbols('sigma_g')
-# # Define symbols for variances of the factors and idiosyncratic error
-# sigma_f, sigma_epsilon = sp.symbols('sigma_f sigma_epsilon')
-# # Step 1: Define the portfolio return formula symbolically
-# portfolio_return_with_g = w.dot(beta * (f + lambda_) + gamma * g + epsilon)
-
-# st.write(r"""
-#          Because the hedging portfolio $h$ is designed to not alter the expected return of the portfolio, the expected return of the portfolio $p$ is the same as the expected return of the initial portfolio $c$:
-#                 """)
-# # Step 2: Take the expectation using sympy.stats
-# expected_portfolio_return_with_g = stats.E(portfolio_return_with_g)
-# st.latex(f"E[r_p] = {sp.latex(expected_portfolio_return_with_g)}")
-
-# # Contribution from the unpriced factor g:
-# # LaTeX: Var_g = (w^\top \gamma)^2 \sigma_g^2
-# variance_g = (w.dot(gamma))**2 * sigma_g**2  # Contribution from unpriced factor g
-# # Contribution from the priced factor f:
-# # LaTeX: Var_f = (w^\top \beta)^2 \sigma_f^2
-# variance_f = (w.dot(beta))**2 * sigma_f**2  # Contribution from priced factor f
-# # Contribution from the idiosyncratic errors:
-# # LaTeX: Var_\epsilon = w^\top w \times \sigma_\epsilon^2
-# variance_epsilon = w.dot(w) * sigma_epsilon**2  # Contribution from idiosyncratic errors
-# # Total variance of the portfolio:
-# total_portfolio_variance_with_g = variance_f + variance_g + variance_epsilon
-
-# st.write(r"""
-#          The hedging portfolio $h$ is designed to reduce the variance of the portfolio by removing the exposure to the unrewarded risk. The variance of the portfolio $p$ is:
-#             """)
-
-# st.latex(f"\\sigma^2_p = {sp.latex(total_portfolio_variance_with_g)}")
-
-
-# st.write(r"""
-#          Which help to improve the Sharpe ratio of the portfolio:
-#             """)
-
-# # Calculate the Sharpe ratio
-# sharpe_ratio_with_g = expected_portfolio_return_with_g / sp.sqrt(total_portfolio_variance_with_g)
-# st.latex(f"\\text{{Sharpe Ratio}} = {sp.latex(sharpe_ratio_with_g)}")
-
-
-# st.subheader('Optimal Hedge Ratio')
-
-# st.write(r"""
-# Daniel $\textit{et al.}$ (2020) showed that we can do better by combining the hedge portfolio $h$ with the characteristic portfolio $c$ in 
-# order to maximize the Sharpe ratio of the portfolio $p$. Given that the hedge portfolio has zero expected excess return, this is equivalent to finding the 
-# combination of the characteristic portfolio and the hedge portfolio that minimizes the variance of the resultant portfolio, that is:
-#          """)
-
-
-# st.latex(r"""
-# \begin{equation}
-#             \min_{\delta} \text{var}(r_c - \delta r_h) \implies \delta^* = \rho_{c,h}\frac{\sigma_c}{\sigma_h}
-# \end{equation}
-#             """)
-
-# st.subheader('Conclusion')
+st.subheader('Conclusion')
