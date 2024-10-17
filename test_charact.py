ac8e4d2f762e258a51bdf8e9a5ff7b79672ecc03
tlorans <thomaslorans@gmail.com>
2024-10-17 15:06:55 +0200

    charact

--- test_charact.py
+++ test_charact.py
@@ -14,7 +14,7 @@ print(data.head())
 print(data.columns)
 
 
-portfolios = (data
+size = (data
   .groupby("date")
   .apply(lambda x: (x.assign(
       portfolio=pd.qcut(
@@ -25,25 +25,45 @@ portfolios = (data
   .groupby(["portfolio","date"])
   .apply(lambda x: np.average(x["R1M_Usd"], weights=x["Mkt_Cap_12M_Usd"]))
   .reset_index(name="ret")
+    .pivot_table(index="date", columns="portfolio", values="ret")
+  .assign(size=lambda x: x["small"]-x["big"])
+  .get(["size"])
 )
 
-print(portfolios.head()) 
-
-portfolios_longshort = (portfolios
-  .pivot_table(index="date", columns="portfolio", values="ret")
-  .assign(long_short=lambda x: x["small"]-x["big"])
+value = (data
+         .assign(
+             booktomarket = lambda x: x["Bv"]/x["Mkt_Cap_12M_Usd"]
+         )
+ .groupby("date")
+  .apply(lambda x: (x.assign(
+      portfolio=pd.qcut(
+        x["booktomarket"], q=[0, 0.3, 0.7, 1], labels=["low","neutral", "high"]))
+    )
+  )
+  .reset_index(drop=True)
+  .groupby(["portfolio","date"])
+  .apply(lambda x: np.average(x["R1M_Usd"], weights=x["Mkt_Cap_12M_Usd"]))
+  .reset_index(name="ret")
+    .pivot_table(index="date", columns="portfolio", values="ret")
+  .assign(value=lambda x: x["high"]-x["low"])
+  .get(["value"])
 )
 
-print(portfolios_longshort.head())
 
-cum_returns = portfolios_longshort['long_short'].add(1).cumprod().subtract(1)
+portfolios = pd.concat([size, value], axis=1)
 
-plot = (ggplot(cum_returns.reset_index(), aes(x='date', y='long_short')) +
-    geom_line() +
-    labs(title="Cumulative Returns of Long-Short Portfolio",
-         x="Date", y="Cumulative Return") +
-    scale_x_datetime(breaks=date_breaks('5 years'), labels=date_format('%Y'))
-    # scale_y_continuous(labels=percent_format())
+cum_returns = portfolios.add(1).cumprod() - 1
+cum_returns_long = cum_returns.reset_index().melt(id_vars='date', value_vars=['size', 'value'], 
+                                                  var_name='factor', value_name='cum_return')
+
+# Plot with different colors for each factor
+plot = (
+    ggplot(cum_returns_long, aes(x='date', y='cum_return', color='factor'))  # Add color by 'factor'
+    + geom_line()  # Use one geom_line to plot both factors
+    + labs(title="Size and Value Sorted Portfolios",
+           x="Date", y="Cumulative Return")
+    + scale_x_datetime(breaks=date_breaks('5 years'), labels=date_format('%Y'))
+    + theme(axis_text_x=element_text(rotation=45, hjust=1))
 )
 
-plot.show()            
+plot.show()
